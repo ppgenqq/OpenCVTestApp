b75190332a30c1251f5031ed730a979d5c9cf242
ppgenqq <ppgenqq@gmail.com>
2018-10-15 20:59:14 +0900

    Initial Commit

---
+++ OpenCVTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCVTestApp
+//
+//  Created by Gen.W on 2018/10/15.
+//  Copyright © 2018年 Gen.W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
